00ffbd8118bd578446ba3e78faa16a8139c8d0d3
Ramazan CESUROĞLU <31744715+csrrmzn@users.noreply.github.com>
2021-03-16 02:17:44 +0300

    Devam Ediyoruz

---
+++ Index.php
@@ -0,0 +1,19 @@
+<?php 
+include "Header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <h1>Kodluyoruz</h1>
+        <h3>Vivense Bootcamp Bitirme Projesi</h3>
+        <h5>İşlemlerinize Üst Menüyü Kullanarak Devam Edebilirsiniz</h5>
+        </div>
+    </div>
+</div>
+
+<?php
+include "Footer.php";
+?>
+
+    
